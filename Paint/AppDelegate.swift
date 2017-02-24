e993006ec62e2b8595e141c6fcb99d9fb2ab6c98
Scalman <waleedhassan@live.se>
2017-02-24 13:24:55 +0100

    Initial Commit

---
+++ Paint/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Paint
+//
+//  Created by Waleed Hassan on 24/02/17.
+//  Copyright © 2017 Scalman & Martin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Paint/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Paint
+//
+//  Created by Waleed Hassan on 24/02/17.
+//  Copyright © 2017 Scalman & Martin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
